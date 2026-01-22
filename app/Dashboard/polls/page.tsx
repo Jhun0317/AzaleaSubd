575ec0ba5db85fb2226ab754ebd2c4e2eb6e5909
Jhun0317 <act.carsido@gmail.com>
2026-01-22 09:33:36 +0800

    fix polls page component import

--- app/Dashboard/polls/page.tsx
+++ app/Dashboard/polls/page.tsx
@@ -1,8 +1,6 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import PollsComponent from "@/components/dashboard/PollsComponent";
-
 export default function PollsPage() {
-  return <PollsComponent />;
+  return null;
 }
