9b0bccbba7dc81939fae264ca1070d2e63e08bd3
Jhun0317 <act.carsido@gmail.com>
2026-01-30 19:09:16 +0800

    fix: remove props from sidebar to fix build error

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,22 @@
 import Sidebar from "@/components/Sidebar";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="flex bg-slate-50 min-h-screen">
-      {/* Removed the props that caused the error */}
-      <Sidebar /> 
+    <div className="flex min-h-screen bg-slate-50">
+      {/* We removed the isOpen/onClose props that caused the build error. 
+          This will use the clean sidebar on the far left. 
+      */}
+      <Sidebar />
 
-      <div className="flex-1 flex flex-col">
-        <main className="p-8">
-          <div className="max-w-6xl mx-auto">
-            {children}
-          </div>
-        </main>
-      </div>
+      <main className="flex-1 p-8">
+        <div className="max-w-6xl mx-auto">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
