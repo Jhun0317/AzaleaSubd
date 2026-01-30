a8ac996f3e79ce67dcc3c2143e4bedd403d728ce
Jhun0317 <act.carsido@gmail.com>
2026-01-30 19:13:28 +0800

    build: nuclear clean of layout and sidebar props

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,12 +7,13 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
-      {/* We removed the isOpen/onClose props that caused the build error. 
-          This will use the clean sidebar on the far left. 
-      */}
-      <Sidebar />
-
-      <main className="flex-1 p-8">
+      {/* This sidebar is fixed to the left */}
+      <div className="hidden lg:block w-72 fixed h-full">
+         <Sidebar />
+      </div>
+      
+      {/* Main Content Area starts after the sidebar width */}
+      <main className="flex-1 lg:ml-72 p-8">
         <div className="max-w-6xl mx-auto">
           {children}
         </div>
