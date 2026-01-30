b72b432a0e087dd1e5b673e3583109aaeddb789a
Jhun0317 <act.carsido@gmail.com>
2026-01-30 14:28:20 +0800

    Remove AvatarImage usage from Profile page

--- pages/Profile.js
+++ pages/Profile.js
@@ -14,7 +14,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Card,
   CardContent,
