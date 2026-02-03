a8b5d6be1dc1844bf3887b711124172f2ac13dc5
Jhun0317 <160616096+Jhun0317@users.noreply.github.com>
2026-02-03 16:07:48 +0800

    Update actions.ts

--- app/(dashboard)/admin/announcements/actions.ts
+++ app/(dashboard)/admin/announcements/actions.ts
@@ -7,14 +7,16 @@ export async function postAnnouncement(formData: FormData) {
   const title = formData.get("title") as string;
   const content = formData.get("content") as string;
 
-  await prisma.announcement.create({
-    data: {
-      title,
-      content,
-    },
-  });
+await prisma.announcement.create({
+  data: {
+    title,
+    content,
+    priority: "normal",
+  },
+});
 
   // This tells the website to refresh the dashboard and tabs
   revalidatePath("/(dashboard)/client/dashboard");
   revalidatePath("/(dashboard)/client/announcements");
 }
+
